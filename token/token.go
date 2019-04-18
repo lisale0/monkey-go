4a57b116b678703b2a17bedb13ffaf167f80f29c
lisale0 <lisa@influxdata.com>
2019-04-17 22:30:08 -0700

    ch 1: lexing

---
+++ token/token.go
@@ -0,0 +1,67 @@
+package monkey
+
+type TokenType string
+
+type Token struct {
+    Type TokenType
+    Literal string
+}
+
+const (
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF"
+
+    // Identifier + literals
+    IDENT = "IDENT"
+    INT = "INT"
+
+    // Operatore
+    ASSIGN = "="
+    PLUS = "+"
+    MINUS = "-"
+    BANG = "!"
+    ASTERISK = "*"
+    SLASH =  "/"
+
+    LT = "<"
+    GT = ">"
+    EQ = "=="
+    NOT_EQ = "!="
+
+    // Delimiters
+    COMMA = ","
+    SEMICOLON = ";"
+
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
+
+    // Keywords
+    FUNCTION = "FUNCTION"
+    LET = "LET"
+    TRUE = "TRUE"
+    FALSE = "FALSE"
+    IF = "IF"
+    ELSE = "ELSE"
+    RETURN = "RETURN"
+)
+
+var keywords = map[string]TokenType {
+    "fn": FUNCTION,
+    "let": LET,
+    "true": TRUE,
+    "false": FALSE,
+    "if": IF,
+    "else": ELSE,
+    "return": RETURN,
+}
+
+// checks the keywords table to see whether the
+// given identifier is in fact a keyword
+func LookupIdent(ident string) TokenType {
+    if tok, ok := keywords[ident]; ok {
+        return tok
+    }
+    return IDENT
+}
